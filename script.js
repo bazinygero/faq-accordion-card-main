d79df9e3c806578f555a39441ff08d114a40b934
bazinygero <97780599+bazinygero@users.noreply.github.com>
2023-01-08 15:36:59 +0200

    js rework

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 document.querySelectorAll('.accordion__question').forEach((item) => {
-	item.addEventListener('click', (event) => {
-		
-		let accordionCollapse = item.nextElementSibling;
+    item.addEventListener('click', (event) => {
 
-		if (!item.classList.contains('collapsing')) {
-			// open item
-			if (!item.classList.contains('open')) {
-				
+        let accordionCollapse = item.nextElementSibling;
+
+        if (!item.classList.contains('collapsing')) {
+            if(!item.classList.contains('open')) {
                 accordionCollapse.style.display = 'block';
-				let accordionHeight = accordionCollapse.clientHeight;
-				console.log(accordionHeight);
+                let accordionHeight = accordionCollapse.clientHeight;
+                console.log(accordionHeight);
 
-				setTimeout(() => {
-					accordionCollapse.style.height = accordionHeight + 'px';
-					accordionCollapse.style.display = '';
-				}, 1);
+                setTimeout(() => {
+                    accordionCollapse.style.height = accordionHeight + 'px';
+                    accordionCollapse.style.display = '';
+                }, 1);
 
-				accordionCollapse.classList = 'accordion__collapse collapsing';
-
-				setTimeout(() => {
-					console.log('open content');
-					accordionCollapse.classList = 'accordion__collapse collapse open';
-				}, 300);
-			}
-			// close item
-			else {
-				
                 accordionCollapse.classList = 'accordion__collapse collapsing';
 
-				setTimeout(() => {
-					accordionCollapse.style.height = '0px';
-				}, 1);
+                setTimeout(() => {
+                    console.log('open content');
+                    accordionCollapse.classList = 'accordion__collapse collapse open';
+                }, 300);
+            } else {
+                accordionCollapse.classList = 'accordion__collapse collapsing';
 
-				setTimeout(() => {
-					console.log('close content');
-					accordionCollapse.classList = 'accordion__collapse collapse';
-					accordionCollapse.style.height = '';
-				}, 300);
-			}
+                setTimeout(() => {
+                    accordionCollapse.style.height = '0px';
+                }, 1);
 
-			item.classList.toggle('open');
-		}
-	});
+                setTimeout(() => {
+                    console.log('close content');
+                    accordionCollapse.classList = 'accordion__collapse collapse';
+                    accordionCollapse.style.height = '';
+                }, 300);
+            }
+
+            item.classList.toggle('open');
+        }
+    });
 });
