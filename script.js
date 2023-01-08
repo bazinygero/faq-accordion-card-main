1c81f3f9f4f8b9b0d79b6df5b24f54d60e50089b
bazinygero <97780599+bazinygero@users.noreply.github.com>
2023-01-08 12:52:14 +0200

    add js functionality

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log(test);
+document.querySelectorAll('.accordion__question').forEach((item) => {
+	item.addEventListener('click', (event) => {
+		
+		let accordionCollapse = item.nextElementSibling;
+
+		if (!item.classList.contains('collapsing')) {
+			// open item
+			if (!item.classList.contains('open')) {
+				
+                accordionCollapse.style.display = 'block';
+				let accordionHeight = accordionCollapse.clientHeight;
+				console.log(accordionHeight);
+
+				setTimeout(() => {
+					accordionCollapse.style.height = accordionHeight + 'px';
+					accordionCollapse.style.display = '';
+				}, 1);
+
+				accordionCollapse.classList = 'accordion__collapse collapsing';
+
+				setTimeout(() => {
+					console.log('open content');
+					accordionCollapse.classList = 'accordion__collapse collapse open';
+				}, 300);
+			}
+			// close item
+			else {
+				
+                accordionCollapse.classList = 'accordion__collapse collapsing';
+
+				setTimeout(() => {
+					accordionCollapse.style.height = '0px';
+				}, 1);
+
+				setTimeout(() => {
+					console.log('close content');
+					accordionCollapse.classList = 'accordion__collapse collapse';
+					accordionCollapse.style.height = '';
+				}, 300);
+			}
+
+			item.classList.toggle('open');
+		}
+	});
+});
